d7d6b6ba0129e704c210e5f1babe5b202873a047
wushen_zhong <1837221153@qq.com>
2024-05-24 18:01:15 +0800

    new file:   .gitignore 	modified:   EasyDesigner.html 	modified:   Oframework.mjs 	modified:   README.md

--- Oframework.mjs
+++ Oframework.mjs
@@ -1,320 +1,304 @@
-class Mutater {
-  eventArgumentName
-  templateElements = new Map()
-  injectList = []
-  #$Proxy
-  allowMutate = true
+const config = {
+  eventArgumentName: 'ev',
 
-  /**
-   * 
-   * @param {object} param0 
-   */
-  constructor({ eventArgumentName = 'ev' }) {
-    if (eventArgumentName !== undefined) {
-      if (!/[$_\w][$_\w\d]*/.test(eventArgumentName))
-        throw new Error(`Invalid identifier of 'eventArgumentName': ${eventArgumentName}`)
-      this.eventArgumentName = eventArgumentName
-    }
-  }
+  defineAttributeName: 'o:define',
+  letAttributeName: 'o:let',
+  onAttributeName: 'o:on',
+  runAttributeName: 'o:run',
+  endAttributeName: 'o:end',
+  backupAttributeName: 'o:backup',
+  injectAttributeName: 'o:inject',
+  shadowAttributeName: 'o:shadow'
+}
 
-  /**
-   * 
-   * @param {Element} beginEle
-   * @param {String} key
-   * @returns {Element|null}
-   */
-  #getHostElement = (beginEle, key) => {
-    for (
-      let ele = beginEle;
-      ele !== null && ele !== undefined;
-      ele = ele.parentElement ?? ele.parentNode?.host
-    )
-      if (Object.hasOwn(ele, key))
-        return ele
-    return null
-  }
+const templateElements = new Map()
+let injectList = []
+const mutatedWeakSet = new WeakSet()
 
-  /**
-   * 
-   * @param {Element} ele 
-   */
-  #convertAttributes = (ele) => {
-    for (const { name, value } of ele.attributes)
-      if (name.startsWith('o:on') && value.startsWith('{'))
-        ele.setAttribute(name, `[function (${this.eventArgumentName}) ${value}]`)
-
-    const runValue = ele.getAttribute('o:run')
-    if (runValue?.startsWith('{'))
-      ele.setAttribute('o:run', `[function () ${runValue}]`)
-  }
-
-  /**
-   * 
-   * @param {Element} ele
-   * @returns {Element}
-   */
-  #extendElement = (ele) => {
-    if (!(ele instanceof Element)) {
-      console.error(ele)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(ele)}.`)
-    }
-
-    const templateEle = this.templateElements.get(ele.tagName.toLowerCase())
-    if (templateEle === undefined)
+/**
+ * 
+ * @param {Element} beginEle
+ * @param {String} key
+ * @returns {Element|null}
+ */
+function getHostElement(beginEle, key) {
+  for (
+    let ele = beginEle;
+    ele !== null && ele !== undefined;
+    ele = ele.parentElement ?? ele.parentNode?.host
+  ) {
+    if (Object.hasOwn(ele, key))
       return ele
-    const parentEle = templateEle.cloneNode(true)
-    parentEle.removeAttribute('o:template')
+    if (Object.hasOwn(ele.dataset, key))
+      return ele.dataset
+  }
+  return null
+}
 
-    for (const { name, value } of ele.attributes) {
-      if (parentEle.hasAttribute(name)) {
-        const parentValue = parentEle.getAttribute(name)
-        if (name == 'o:def' || name.startsWith('o:on') || name == 'o:run')
-          parentEle.setAttribute(name, parentValue.slice(0, -1) + ', ' + value.slice(1))
-        else if (name == 'class')
-          parentEle.setAttribute(name, parentValue + ' ' + value)
-        else
-          parentEle.setAttribute(name, parentValue + value)
-      } else {
-        parentEle.setAttribute(name, value)
-      }
-    }
+/**
+ * 
+ * @param {Element} ele 
+ */
+function convertAttributes(ele) {
+  for (const { name, value } of ele.attributes)
+    if (name.startsWith(config.onAttributeName) && value.startsWith('{'))
+      ele.setAttribute(name, `[function (${config.eventArgumentName}) ${value}]`)
 
-    if (parentEle.hasAttribute('o:shadow')) {
-      const shadowOptions = Function('return ' + parentEle.getAttribute('o:shadow')).call(ele, this.#$Proxy)
-      const shadowRoot = parentEle.attachShadow(shadowOptions)
-      shadowRoot.append(...parentEle.childNodes)
-      parentEle.append(...ele.childNodes)
-    } else {
-      for (const slotEle of parentEle.querySelectorAll('slot[name]')) {
-        const elesHasSlot = ele.querySelectorAll(`*[slot="${slotEle.name}"]`)
-        elesHasSlot.forEach(eleHasSlot => eleHasSlot.removeAttribute('slot'))
-        slotEle.replaceWith(...elesHasSlot)
-      }
-      const defaultSlotEle = parentEle.querySelector('slot:not([name])')
-      if (defaultSlotEle)
-        defaultSlotEle.replaceWith(...ele.childNodes)
-      else
-        parentEle.append(...ele.childNodes)
-    }
+  const orunValue = ele.getAttribute(config.runAttributeName)
+  if (orunValue?.startsWith('{'))
+    ele.setAttribute(config.runAttributeName, `[function () ${orunValue}]`)
 
-    return this.#extendElement(parentEle)
+  const oendValue = ele.getAttribute(config.endAttributeName)
+  if (oendValue?.startsWith('{'))
+    ele.setAttribute(config.endAttributeName, `[function () ${oendValue}]`)
+}
+
+/**
+ * 
+ * @param {Element} ele
+ * @returns {Element}
+ */
+function extendElement(ele, $proxy) {
+  if (!(ele instanceof Element)) {
+    console.error(ele)
+    throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(ele)}.`)
   }
 
-  #checkAttribute = (ele, name, value) => {
-    if (!(
-      (value.startsWith('{') && value.endsWith('}'))
-      || (value.startsWith('[') && value.endsWith(']'))
-    )) {
-      console.error(ele)
-      throw new SyntaxError(`Invalid attribute value of ${name}: '${value}'.`)
-    }
-  }
-
-  /**
-   * 
-   * @param {Element} ele
-   * @param {string} key
-   * @param {} value
-   */
-  #defineVariable = (ele, key, value) => {
-    Object.defineProperty(ele, key, {
-      get: () => value,
-      set: (v) => {
-        value = v
-        this.injectList = this.injectList.filter(([prop, weakText, injectCode, $Proxy]) => {
-          const text = weakText.deref()
-          if (text === undefined)
-            return false
-          if (ele.contains(text) && key == prop) {
-            const hostEle = this.#getHostElement(text, prop)
-            if (ele === hostEle)
-              text.nodeValue = Function('$', `return \`${injectCode}\``).call(text, $Proxy)
-          }
-          return true
-        })
-      }
-    })
-  }
-
-  /**
-   * 
-   * @param {Element} ele
-   * @returns {Element|null}
-   */
-  #mutateElement = (ele) => {
-    if (!(ele instanceof Element)) {
-      console.error(ele)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(ele)}.`)
-    }
-
-    if (ele.mutated)
-      return ele
-
-    if (ele.tagName == 'SCRIPT')
-      return ele
-
-    this.#convertAttributes(ele)
-
-    if (ele.hasAttribute('o:template')) {
-      for (const templateEle of [ele, ...ele.querySelectorAll('*[o\\:template]')]) {
-        this.templateElements.set(templateEle.getAttribute('o:template'), templateEle)
-        templateEle.remove()
-      }
-      return null
-    }
-
-    ele.mutated = true
-
-    if (ele.hasAttribute('style')) {
-      ele.setAttribute('style', ele.getAttribute('style') + '; display: none;')
-    } else {
-      var noStyle = true
-      ele.setAttribute('style', '; display: none;')
-    }
-
-    const extendedEle = this.#extendElement(ele)
-    if (extendedEle !== ele) {
-      extendedEle.mutated = true
-      ele.replaceWith(extendedEle)
-      ele = extendedEle
-    }
-
-    this.#$Proxy = new Proxy({}, {
-      get: (_target, p) => this.#getHostElement(ele, p)[p],
-      set: (_target, p, newValue) => this.#getHostElement(ele, p)[p] = newValue
-    })
-
-    if (ele.hasAttribute('o:def')) {
-      const odefAttrValue = ele.getAttribute('o:def')
-
-      this.#checkAttribute(ele, 'o:def', odefAttrValue)
-
-      const defObj = Function('$', `return ${odefAttrValue}`).call(ele, this.#$Proxy)
-      for (let [key, value] of Object.entries(defObj))
-        this.#defineVariable(ele, key, value)
-    }
-
-    for (const { name, value } of ele.attributes) {
-      if (name.startsWith('o:on')) {
-        this.#checkAttribute(ele, name, value)
-
-        Function('$', `return ${value}`).call(ele, this.#$Proxy)
-          .reverse()
-          .forEach(handle => ele.addEventListener(name.slice(4), handle))
-      }
-    }
-
-    ele.setAttribute('style', ele.getAttribute('style').slice(0, -16))
-    if (noStyle)
-      ele.removeAttribute('style')
-
-    if (ele.hasAttribute('o:run')) {
-      const orunAttrValue = ele.getAttribute('o:run')
-
-      this.#checkAttribute(ele, 'o:run', orunAttrValue)
-
-      Function('$', `return ${orunAttrValue}`).call(ele, this.#$Proxy)
-        .reverse()
-        .forEach(handle => handle.call(ele))
-    }
-
+  const templateEle = templateElements.get(ele.tagName.toLowerCase())
+  if (templateEle === undefined)
     return ele
-  }
+  const parentEle = templateEle.cloneNode(true)
+  parentEle.removeAttribute(config.defineAttributeName)
 
-  /**
-   * 
-   * @param {Text} text
-   * @returns {Text}
-   */
-  #mutateText = (text) => {
-    if (!(text instanceof Text)) {
-      console.error(text)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(text)}.`)
-    }
-
-    if (text.mutated)
-      return text
-
-    text.mutated = true
-
-    Function('$', `return \`${text.nodeValue}\``).call(text, new Proxy({}, {
-      get: (_target, p) => this.injectList.push([p, new WeakRef(text), text.nodeValue, this.#$Proxy])
-    }))
-
-    text.nodeValue = Function('$', `return \`${text.nodeValue}\``).call(text, this.#$Proxy)
-
-    return text
-  }
-
-  /**
-   * 
-   * @param {Node} node
-   * @returns {Node|null}
-   */
-  #mutateNode = (node) => {
-    if (!node instanceof Node) {
-      console.error(node)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(node)}.`)
-    }
-
-    if (node.mutated)
-      return node
-
-    switch (node.nodeType) {
-      case Node.ELEMENT_NODE:
-        return this.#mutateElement(node)
-      case Node.TEXT_NODE:
-        return this.#mutateText(node)
-      default:
-        return node
+  for (const { name, value } of ele.attributes) {
+    if (parentEle.hasAttribute(name)) {
+      const parentValue = parentEle.getAttribute(name)
+      if (name == config.letAttributeName || name.startsWith(config.onAttributeName) || name == config.runAttributeName)
+        parentEle.setAttribute(name, parentValue.slice(0, -1) + ', ' + value.slice(1))
+      else if (name == 'class')
+        parentEle.setAttribute(name, parentValue + ' ' + value)
+      else
+        parentEle.setAttribute(name, parentValue + value)
+    } else {
+      parentEle.setAttribute(name, value)
     }
   }
 
-  /**
-   * 
-   * @param {Node} node 
-   * @returns 
-   */
-  #mutateAll = (node) => {
-    if (!node instanceof Node) {
-      console.error(node)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(node)}.`)
+  if (parentEle.hasAttribute(config.shadowAttributeName)) {
+    const shadowOptions = Function('return ' + parentEle.getAttribute(config.shadowAttributeName)).call(ele, $proxy)
+    const shadowRoot = parentEle.attachShadow(shadowOptions)
+    shadowRoot.append(...parentEle.childNodes)
+    parentEle.append(...ele.childNodes)
+  } else {
+    for (const slotEle of parentEle.querySelectorAll('slot[name]')) {
+      const elesHasSlot = ele.querySelectorAll(`*[slot="${slotEle.name}"]`)
+      elesHasSlot.forEach(eleHasSlot => eleHasSlot.removeAttribute('slot'))
+      slotEle.replaceWith(...elesHasSlot)
     }
-
-    node = this.#mutateNode(node)
-
-    node?.shadowRoot?.childNodes?.forEach(this.#mutateAll)
-    node?.childNodes?.forEach(this.#mutateAll)
-
-    return node
+    const defaultSlotEle = parentEle.querySelector('slot:not([name])')
+    if (defaultSlotEle)
+      defaultSlotEle.replaceWith(...ele.childNodes)
+    else
+      parentEle.append(...ele.childNodes)
   }
 
-  /**
-   * 
-   * @param {Node} node 
-   * @returns 
-   */
-  mutate = (node) => {
-    if (!node instanceof Node) {
-      console.error(node)
-      throw new TypeError(`Invalid argument: ${Object.prototype.toString.call(node)}.`)
-    }
+  return extendElement(parentEle, $proxy)
+}
 
-    if (!this.allowMutate)
-      throw new Error(`This function is allowed to be called only once.`)
-    this.allowMutate = false
-
-    node = this.#mutateAll(node)
-
-    new MutationObserver(mutationRecords => {
-      mutationRecords.forEach(mutationRecord => {
-        if (mutationRecord.type == 'childList')
-          mutationRecord.addedNodes.forEach(addedNode => this.#mutateAll(addedNode))
-      })
-    }).observe(node, { childList: true, subtree: true })
-
-    return node
+/**
+ * 
+ * @param {Element} ele
+ * @param {string} name
+ * @param {string} value
+ */
+function checkAttribute(ele, name, value) {
+  if (!(
+    (value.startsWith('{') && value.endsWith('}'))
+    || (value.startsWith('[') && value.endsWith(']'))
+  )) {
+    console.error(ele)
+    throw new SyntaxError(`Invalid attribute value of ${name}: '${value}'.`)
   }
 }
 
-export default Mutater
+/**
+ * 
+ * @param {Element} ele
+ * @param {string} key
+ * @param {} value
+ */
+function defineVariable(ele, key, value) {
+  Object.defineProperty(ele, key, {
+    get: () => value,
+    set: (v) => {
+      value = v
+      injectList = injectList.filter(([weakEle, _usedKey]) => weakEle.deref() !== undefined)
+      injectList
+        .map(([weakEle, usedKey]) => [weakEle.deref(), usedKey])
+        .filter(([eleHasOinject, usedKey]) => ele.contains(eleHasOinject) && key == usedKey)
+        .forEach(([eleHasOinject, usedKey]) => {
+          for (
+            let hostEle = eleHasOinject;
+            hostEle !== null && hostEle !== undefined;
+            hostEle = hostEle.parentElement ?? hostEle.parentNode?.host
+          ) {
+            if (Object.hasOwn(hostEle, usedKey) && ele === hostEle) {
+              eleHasOinject.innerHTML = Function('$', `return \`${eleHasOinject.getAttribute(config.injectAttributeName)}\``).call(eleHasOinject, new Proxy({}, {
+                get: (_target, p) => getHostElement(eleHasOinject, p)[p],
+                set: (_target, p, newValue) => getHostElement(eleHasOinject, p)[p] = newValue
+              }))
+            }
+          }
+        })
+    }
+  })
+}
+
+function hideElement(ele) {
+  if (ele.hasAttribute('style')) {
+    ele.setAttribute('style', ele.getAttribute('style') + '; display: none;')
+    return true
+  } else {
+    ele.setAttribute('style', '; display: none;')
+    return false
+  }
+}
+
+/**
+ * 
+ * @param {Element} ele
+ */
+function mutate(ele) {
+  if (mutatedWeakSet.has(ele))
+    return ele
+
+  if (ele.tagName == 'SCRIPT')
+    return null
+
+  if (ele.hasAttribute(config.defineAttributeName)) {
+    let obackup
+    if (ele.parentElement.hasAttribute(config.backupAttributeName))
+      obackup = JSON.parse(ele.parentElement.getAttribute(config.backupAttributeName))
+    else
+      obackup = []
+    const parentEle = ele.parentElement
+    const documentFragment = document.createDocumentFragment()
+    for (const templateEle of [ele, ...ele.querySelectorAll('*[o\\:template]')]) {
+      convertAttributes(templateEle)
+      documentFragment.append(templateEle)
+    }
+    for (const templateEle of documentFragment.children) {
+      templateElements.set(templateEle.getAttribute(config.defineAttributeName), templateEle)
+      obackup.push(templateEle.outerHTML)
+    }
+    parentEle.setAttribute(config.backupAttributeName, JSON.stringify(obackup))
+    return null
+  }
+
+  if (ele.hasAttribute(config.backupAttributeName)) {
+    const documentFragment = document.createElement('div')
+    documentFragment.innerHTML = JSON.parse(ele.getAttribute(config.backupAttributeName)).join('')
+    for (const templateEle of documentFragment.children)
+      templateElements.set(templateEle.getAttribute(config.defineAttributeName), templateEle)
+  }
+
+  if (ele.hasAttribute('style')) {
+    ele.setAttribute('style', ele.getAttribute('style') + '; display: none;')
+    var noStyle = false
+  } else {
+    ele.setAttribute('style', '; display: none;')
+    var noStyle = true
+  }
+
+  convertAttributes(ele)
+
+  mutatedWeakSet.add(ele)
+
+  const $proxy = new Proxy({}, {
+    get: (_target, p) => getHostElement(ele, p)[p],
+    set: (_target, p, newValue) => getHostElement(ele, p)[p] = newValue
+  })
+
+  const extendedEle = extendElement(ele, $proxy)
+  if (extendedEle !== ele) {
+    mutatedWeakSet.add(extendedEle)
+    ele.replaceWith(extendedEle)
+    ele = extendedEle
+  }
+
+  if (ele.hasAttribute(config.letAttributeName)) {
+    const odefValue = ele.getAttribute(config.letAttributeName)
+    checkAttribute(ele, config.letAttributeName, odefValue)
+    const defObj = Function('$', `return ${odefValue}`).call(ele, $proxy)
+    for (const [key, value] of Object.entries(defObj))
+      defineVariable(ele, key, value)
+  }
+
+  const onAttributeNameLength = config.onAttributeName.length
+  for (const { name, value } of ele.attributes) {
+    if (name.startsWith(config.onAttributeName)) {
+      checkAttribute(ele, name, value)
+      Function('$', `return ${value}`)
+        .call(ele, $proxy)
+        .reverse()
+        .forEach(handle => ele.addEventListener(name.slice(onAttributeNameLength), handle))
+    }
+  }
+
+  ele.setAttribute('style', ele.getAttribute('style').slice(0, -16))
+  if (noStyle)
+    ele.removeAttribute('style')
+
+  if (ele.hasAttribute(config.runAttributeName)) {
+    const orunValue = ele.getAttribute(config.runAttributeName)
+    checkAttribute(ele, config.runAttributeName, orunValue)
+    Function('$', `return ${orunValue}`)
+      .call(ele, $proxy)
+      .reverse()
+      .forEach(handle => handle.call(ele))
+  }
+
+  if (ele.hasAttribute(config.injectAttributeName)) {
+    let oinjectValue = ele.getAttribute(config.injectAttributeName)
+    if (oinjectValue == '') {
+      oinjectValue = ele.innerHTML
+      ele.setAttribute(config.injectAttributeName, oinjectValue)
+    }
+    Function('$', `return \`${oinjectValue}\``).call(ele, new Proxy({}, {
+      get: (_target, p) => injectList.push([new WeakRef(ele), p])
+    }))
+    ele.innerHTML = Function('$', `return \`${oinjectValue}\``).call(ele, $proxy)
+  }
+
+  if (ele.shadowRoot !== null)
+    for (const child of [...ele.shadowRoot.children])
+      mutate(child)
+  for (const child of [...ele.children])
+    mutate(child)
+
+  if (ele.hasAttribute(config.endAttributeName)) {
+    const oendValue = ele.getAttribute(config.endAttributeName)
+    checkAttribute(ele, config.endAttributeName, oendValue)
+    Function('$', `return ${oendValue}`)
+      .call(ele, $proxy)
+      .reverse()
+      .forEach(handle => handle.call(ele))
+  }
+
+  return ele
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  new MutationObserver(mutationRecords => {
+    mutationRecords.forEach(mutationRecord => {
+      mutationRecord.addedNodes.forEach(addedNode => {
+        if (addedNode.nodeType === Node.ELEMENT_NODE)
+          mutate(addedNode)
+      })
+    })
+  }).observe(document.body, { childList: true, subtree: true })
+
+  mutate(document.body)
+})
+
+export default config
--- README.md
+++ README.md
@@ -1,30 +1,41 @@
 # Oframework
 
-Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架。
+Just a simple HTML / JavaScript framework. 一个简单的 HTML/JavaScript 框架。
 
 ## 示例
 
 ### [EasyDesigner.html](./EasyDesigner.html)
 
-简单的网页设计程序，使用TaiwindCSS
+简单的网页设计程序，使用 TaiwindCSS
 
 ## 文档
 
+全局引入 Oframework ，并通过`config`对象修改属性名默认值
+
+```html
+<script src="./Oframework.mjs" type="module"></script>
+<script type="module">
+  import config from './Oframework.mjs'
+  config.defineAttributeName = 'o:template'
+  config.letAttributeName = 'o:define'
+</script>
+```
+
 ### 标签属性
 
-#### `o:def`
+#### `o:let`
 
 在`div`对象上定义num变量
 
 ```html
-<div o:def="{ num: 1 }"></div>
+<div o:let="{ num: 1 }"></div>
 ```
 
-`o:def`、`o:on`、`o:run`、`o:shadow`属性值中都可以使用`$`对象来获取父元素链上定义的变量
+`o:let`、`o:on`、`o:run`、`o:end`、`o:shadow`属性值中都可以使用`$`对象来获取父元素链上定义的变量
 
 ```html
-<div o:def="{ num: 1 }">
-  <div o:def="{ fn: function () { return $.num } }"></div>
+<div o:let="{ num: 1 }">
+  <div o:let="{ fn: function () { return $.num } }"></div>
 </div>
 ```
 
@@ -37,44 +48,44 @@ Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架�
 ```
 
 > [!NOTE]
-> 如此定义的`o:def`、`o:on`、`o:run`属性值中不应含有前导或尾随空字符
+> 如此定义的`o:let`、`o:on`、`o:run`、`o:end`属性值中不应含有前导或尾随空字符
 
-在相同的事件上定义多个监听器
+在相同的事件上定义多个监听器，将按逆序添加
 
 ```html
 <div o:onclick="[function () { console.log(1) }, function () { console.log(2) }]"></div>
 ```
 
-将按逆序添加，但并不推荐主动使用这种方法，尽量通过继承自动实现
+并不推荐主动使用这种方法，尽量通过继承自动实现
 
-#### `o:run`
+#### `o:run`，`o:end`
 
-定义直接运行的代码块， 将在转化完成后立即执行
+`o:run`定义直接运行的代码块，将在转化完成后立即执行，此时子元素尚未转化
+`o:end`定义最终运行的代码块，将在所有子元素转化完成后执行
 
 ```html
 <div o:run="{ console.log(1) }"></div>
+<div o:end="{ console.log(1) }"></div>
 ```
-
-> [!NOTE]
-> 此时子元素尚未转化
 
 也可定义函数列，将按逆序执行
 
 ```html
 <div o:run="[function () { console.log(2) }, function () { console.log(1) }]"></div>
+<div o:end="[function () { console.log(2) }, function () { console.log(1) }]"></div>
 ```
 
 并不推荐主动使用这种方法，尽量通过继承自动实现
 
-#### `o:template`
+#### `o:define`
 
-定义一个模板标签，它将保存到`oxygen.templateElements`并从文档中移除
+定义一个模板标签，完成定义后将从文档中移除
 
 ```html
-<div o:template="bob"></div>
+<div o:define="bob"></div>
 ```
 
-然后可以像这样来继承模板标签
+然后可以像这样来实例化继承模板标签
 
 ```html
 <bob></bob>
@@ -83,17 +94,17 @@ Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架�
 也可实现标签的继承链
 
 ```html
-<bob o:template="john"></bob>
+<bob o:define="john"></bob>
 <john></join>
 ```
 
-继承时模板标签上的所有属性都将转移合并到实例化的标签上，模板元素的属性值在前，实例化标签的属性值在后，对于该模块定义的新属性，将自动转化为可识别的样式
+继承时模板标签上的所有属性都将转移合并到实例化的标签上，模板元素的属性值在前，实例化标签的属性值在后，对于该模块定义的新属性，将自动转化为可识别的属性值
 
 如定义：
 
 ```html
-<div o:template="bob" class="monica" o:onclick="{ console.log(2) }"></div>
-<bob o:template="john" class="van"></bob>
+<div o:define="bob" class="monica" o:onclick="{ console.log(2) }"></div>
+<bob o:define="john" class="van"></bob>
 ```
 
 ```html
@@ -114,7 +125,7 @@ Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架�
 对于模板标签的实例化方法，如果存在`o:shadow`属性，则其值将作为`attachShadow`的参数，其中`clonable`的默认值为`true`，模板标签内所有节点都将附加到模板标签的`ShadowRoot`中
 
 ```html
-<div o:template="bob" o:shadow="{ mode: 'open' }">
+<div o:define="bob" o:shadow="{ mode: 'open' }">
   <div class="van"><slot></slot></div>
 </div>
 ```
@@ -140,7 +151,7 @@ Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架�
 若模板标签中有不具`name`属性的`slot`元素，则将其替换实例化标签内的剩余节点，否则将剩余节点直接添加到父元素尾部
 
 ```html
-<div o:template="bob" class="van">
+<div o:define="bob" class="van">
   <slot name="new"></slot>
   <slot></slot>
 </div>
@@ -162,57 +173,66 @@ Just a simple HTML / JavaScript framework. 一个简单的HTML/JavaScript框架�
 </div>
 ```
 
-### 标签文本
+#### `o:backup`
 
-所有文本将使用` `` `处理，意味着文本中可以使用`${}`语法，在其中使用`$`对象读取变量时将会保存一个引用，每当对应变量被赋值，使用该变量的文本会被重新计算
+所有`o:define`定义的新标签将会先以字符串数组的形式储存在定义时的父标签`o:backup`属性中，并从文档中移除
+
+转化含有`o:backup`属性的标签时会解析其值并定义新标签。由于`o:define`定义新标签时父标签已经完成转化，所以不会重复定义
+
+#### `o:inject`
+
+定义将要注入（赋值到`innerHTML`）的 HTML 文本，将使用 JavaScript 中的` `` `处理，意味着文本中可以使用`${}`语法，在其中使用`$`对象读取变量时将会保存一个引用，每当对应变量被赋值，使用该变量的文本会被重新计算并注入
+
+若为空值，将先把`innerHTML`的值赋值为`o:inject`的属性值
 
 ```html
-<div o:def="{ num: 1 }">
+<div o:let="{ num: 1 }" inject>
   ${$.num}
 </div>
 ```
 
-<!-- ## 方法 -->
+等价于
+
+```html
+<div o:let="{ num: 1 }" inject="\n  ${$.num}\n"></div>
+```
 
 ### JavaScript API
 
-### `Oxygen`
+### `config`
 
-#### `new Oxygen(options)`
+#### `config.eventArgumentName` \<string>
 
-- `options` \<Object>
-  - `eventArgumentName` \<string>
+`o:on`定义的事件监听器方法中的事件对象名
 
-#### `oxygen.eventArgumentName` \<string>
+#### `config.defineAttributeName` \<string>
 
-定义事件监听器时事件对象的名称。默认值：`'ev'`
+定义新标签的属性名，默认值：`o:define`
 
-#### `oxygen.templateElements` \<Map>
+#### `config.letAttributeName` \<string>
 
-可以手动添加模板元素。键为新标签名，值为模板元素
+定义变量的属性名，默认值：`o:let`
 
-#### `oxygen.mutateAll(node)`
+#### `config.onAttributeName` \<string>
 
-- `node` \<Node>
-- Returns: \<Node> | null
+定义事件监听器的属性名的开头，默认值：`o:on`
 
-转化节点及其后代节点，如下例，同时可以添加`MutationObserver`来监听元素的实时更改
+#### `config.runAttributeName` \<string>
 
-```js
-const oxygen = new Oxygen()
-const root = oxygen.mutateAll(document.querySelector('#root'))
+定义立刻执行方法的属性名，默认值：`o:run`
 
-new MutationObserver(mutationRecords => {
-  mutationRecords.forEach(mutationRecord => {
-    mutationRecord.addedNodes.forEach(addedNode => oxygen.mutateAll(addedNode))
-  })
-}).observe(root, { childList: true, subtree: true })
-```
+#### `config.endAttributeName` \<string>
 
-主方法，转化节点及其后代节点，同时添加`MutationObserver`来监听元素的实时更改，该方法只能调用一次，如下例
+定义最终执行方法的属性名，默认值：`o:end`
 
-```js
-import Mutater from './Oframework.mjs'
-const mutater = new Mutater()
-const root = mutater.mutateAll(document.querySelector('#root'))
-```
+#### `config.backupAttributeName` \<string>
+
+储存模板标签数组的属性名，默认值：`o:backup`
+
+#### `config.injectAttributeName` \<string>
+
+定义注入富文本的属性名，默认值：`o:inject`
+
+#### `config.shadowAttributeName` \<string>
+
+定义`attachShadow`的参数的属性名，默认值：`o:shadow`
